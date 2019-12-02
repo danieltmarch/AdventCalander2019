e9cca7a2fc7f16711112fcbebbbb67e5079b4e60
Daniel March <danieltmarch@gmail.com>
2019-12-01 19:33:51 -0800

    Added general coments.

--- adventCalander/adventCalander.cpp
+++ adventCalander/adventCalander.cpp
@@ -4,8 +4,10 @@
 #include <iostream>
 using namespace std;
 
+//function prototype
 int handleMass(int, int	);
 
+//main() is the entry point of the program
 int main()
 {
 	int mass = 1; //mass of the rocket
@@ -13,12 +15,13 @@ int main()
 	while (mass != 0)
 	{
 		cout << "mass: ";
-		cin >> mass;
+		cin >> mass; // get input
 		fuelTotal = handleMass(mass, fuelTotal);
 		cout << " Fuel Total: " << fuelTotal << endl;
 	}
 }
 
+//adds mass of a module and subsequent fuel mass until fuel mass is 0 or less.
 int handleMass(int mass, int sum)
 {
 	int fuel = (mass / 3) - 2;
